fa5d2db5414482c116df077fdc09a7d18c6043a8
Koushik <55914356+koushik147@users.noreply.github.com>
2022-11-22 03:03:35 -0500

    Add files via upload

---
+++ DynamicProgramming5/JumpGame2.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start = 0 # declaring the start value with 0
+        end = 0 # declaring the end value with 0
+        jumps = 0 # declaring the jumps value with 0
+        
+        while end<len(nums)-1: # run until the length of nums is greater than end
+            farthest = 0 
+            for i in range(start,end+1): # run until the loop and set the farthest with the maximum between farthest and nums of ith value
+                farthest = max(farthest,i+nums[i])
+
+            jumps+=1 # increment by 1
+            start = end+1 # assign end to start
+            end = farthest # assign farthest to end
+        return jumps # returning the jumps
---
+++ DynamicProgramming5/LongestCommonSubsequence.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        m = len(text1) # length of text1
+        n = len(text2) # length of text2
+        grid = [[0 for i in range(n+1)] for j in range(m+1)] # creating the grid value with 0
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1): # run until the loop reaches the last cell and check whether text1 is equal to text2 then add the grid [i][j] with 1
+                if text1[i]==text2[j]:
+                    grid[i][j] = 1+ grid[i+1][j+1]
+
+                else:
+                    grid[i][j] += max(grid[i][j+1],grid[i+1][j]) # assign the grid[i][j] with the maximum value between the grid[i+1][j] and grid[i][j+1]
+
+
+        return grid[0][0] # return the grid starting value
+        
+
+
+
